6117290c8ae87f7369cdde99f53ff026d0dcd0ac
ase1590 <ase1590@hotmail.com>
2024-05-16 13:56:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("explodeBtn").addEventListener("click", function() {
+    explode();
+});
+
+function explode() {
+    var explodeBtn = document.getElementById("explodeBtn");
+    explodeBtn.style.animation = "explodeAnimation 1s forwards"; // Adjust animation duration to 5 seconds
+    
+    setTimeout(function() {
+        explodeBtn.style.display = "none";
+        showCongrats();
+    }, 1000); // Adjust the delay to match animation duration
+}
+
+
+function showCongrats() {
+    var congratsText = document.getElementById("congratsText");
+    congratsText.style.display = "block";
+    congratsText.innerHTML = "Subbed to!<br>OnlyWeigans!";
+}
